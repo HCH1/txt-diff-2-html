39fea0b7df4bda3f6e7c5c79baf0b8b5072502e1
HCH1 <theone_730801@hotmail.com>
2016-10-18 12:38:11 +0800

    Update SOPcleanDMforDiffv2.sh

--- SOPcleanDMforDiffv2.sh
+++ SOPcleanDMforDiffv2.sh
@@ -1,5 +1,10 @@
 egrep -v "kw1|kw2" i1 > o1
 egrep -v "kw1|kw2" i2 > o2
+
 diff -b o1 o2 > o3b
 diff -u o1 o2 | cdiff > o1o2
+
 python demo_difflib.py > dl1
+
+egrep "<\ " o14b > o14bBF
+egrep ">\ " o14b > o14bAF
