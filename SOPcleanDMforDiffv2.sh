221d407684e4fcb0429f8f2542c1586cb388f842
HCH1 <theone_730801@hotmail.com>
2016-10-21 15:04:35 +0800

    Update SOPcleanDMforDiffv2.sh

--- SOPcleanDMforDiffv2.sh
+++ SOPcleanDMforDiffv2.sh
@@ -8,3 +8,15 @@ python demo_difflib.py > dl1
 
 egrep "<\ " o14b > o14bBF
 egrep ">\ " o14b > o14bAF
+## .sh
+python demo_xmlSplitV3.py > 1021drc01o1_noBKT
+sed '/[[:space:]]$/d' 1021drc01o1_noBKT > 1021drc01o1_noBKT_nospc
+## demo_xmlSplitV3.py
+# -*- coding: utf-8 -*-
+import re
+path = '/tdhome/hunghc/aa.xml'
+file = open(path, "r")    
+line = file.read()    
+exp = re.compile(r'<.*?>')
+text_only = exp.sub('',line).strip() #for all <*>, do repl as ''
+print text_only
